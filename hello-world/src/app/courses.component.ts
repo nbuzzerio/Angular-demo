9a021ab313bfff8f0b122156d0e519c8f64242f5
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-03-02 21:58:45 -0500

    Implemented template with string interpolation

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', //references css selectors --> <course> = "courses" | <dive class=courses> = ".courses" | <div id=courses> = "#courses"
-    template: '<h2>Courses</h2>' //html markup to be rendered in the component
+    template: '<h2>{{"Title: " + title}}</h2>' //html markup to be rendered in the component
 })
 export class CoursesComponent {
-    
+    title = "List of Courses"
+
+    getTitle() {
+        return this.title;
+    }
 }
