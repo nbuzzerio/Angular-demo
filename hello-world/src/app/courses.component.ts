32babd541c30fed44491d32aa2830a8fcd379334
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-03-02 22:12:00 -0500

    used Directives to create listItems on the DOM

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', //references css selectors --> <course> = "courses" | <dive class=courses> = ".courses" | <div id=courses> = "#courses"
-    template: '<h2>{{"Title: " + title}}</h2>' //html markup to be rendered in the component
+    template: `
+        <h2>{{title}}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
+    //prefix Directuves with an *
+    //use ngFor to create attributes
 })
 export class CoursesComponent {
-    title = "List of Courses"
+    title = "List of Courses";
 
-    getTitle() {
-        return this.title;
-    }
+    courses = ["course 1", "course 2", "course 3"];
 }
